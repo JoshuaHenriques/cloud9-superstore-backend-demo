80f2572189fc9d3b6b160f46025c221f85c7a106
joshuahenriques <joshuahenriques95@gmail.com>
2021-06-22 21:46:52 -0400

    Implemented Cart Model

--- src/main/java/org/jayhenri/ecommerce/model/Cart.java
+++ src/main/java/org/jayhenri/ecommerce/model/Cart.java
@@ -4,10 +4,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.UUID;
+import org.javatuples.Triplet;
 
 // TODO: Implement HST+GST+PROMOCODE computation
 
@@ -19,16 +19,38 @@ public class Cart {
     @Id
     private UUID uuid;
 
-    private ArrayList<Item> items;
+    private ArrayList<Triplet<Item, Integer, Character>> items;
 
     private Double subTotal;
 
     private Double total;
 
-    private final Double HST = 0.13;
+    private static final Double HST = 0.13;
 
-    public Cart(UUID uuid, ArrayList<Item> items) {
+    private static final Double DELIVERY_FEE = 10.00;
+
+    public Cart(UUID uuid) {
         this.uuid = uuid;
-        this.items = items;
+    }
+
+    public void addToCart(Item item, Integer quantity, Character size) {
+        items.add(new Triplet<Item, Integer, Character>(item, quantity, size));
+        update();
+    }
+
+    public void removeFromCart(String itemName) {
+        items.forEach(item -> {
+            if (item.getValue0().getName().equals(itemName)) {
+                items.remove(item);
+            }
+        });
+        update();
+    }
+
+    public void update() {
+        items.forEach(item -> {
+            subTotal += item.getValue0().getPrice();
+        });
+        total = subTotal*HST + DELIVERY_FEE;
     }
 }
