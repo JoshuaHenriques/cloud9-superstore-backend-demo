9538abbb14643995f6815d9db5384b9c6ab03c21
joshuahenriques <joshuahenriques95@gmail.com>
2021-07-26 15:16:08 -0400

    commit

--- src/main/resources/sql/models.sql
+++ src/main/resources/sql/models.sql
@@ -1,5 +1,5 @@
 CREATE TABLE address (
-	address_id      uuid DEFAULT uuid_generate_v4();
+	address_id      UUID			NOT NULL IDENTITY PRIMARY KEY DEFAULT uuid_generate_v4(),
 	street_name     VARCHAR(50)     NOT NULL,
 	street_number   SMALLINT        NOT NULL,
 	unit_number     SMALLINT        NOT NULL,
@@ -9,20 +9,20 @@ CREATE TABLE address (
 );
 
 CREATE TABLE cart (
-	cart_id     uuid                DEFAULT uuid_generate_v4();
+	cart_id     UUID                NOT NULL IDENTITY PRIMARY KEY DEFAULT uuid_generate_v4(),
 	email       VARCHAR(100)        NOT NULL,
 	total       DOUBLE PRECISION    NOT NULL
 );
 
 CREATE TABLE creditcard (
-	creditcard_id   uuid            DEFAULT uuid_generate_v4();
+	creditcard_id   UUID            NOT NULL IDENTITY PRIMARY KEY DEFAULT uuid_generate_v4(),
 	first_name      VARCHAR(50)     NOT NULL,
 	last_name       VARCHAR(50)		NOT	NULL,
 	ccn				VARCHAR(20)		NOT NULL,
 );
 
 CREATE TABLE item (
-	item_id			uuid				DEFAULT uuid_generate_v4();
+	item_id			UUID				NOT NULL IDENTITY PRIMARY KEY DEFAULT uuid_generate_v4(),
 	item_name		VARCHAR(25)			NOT NULL,
 	description		VARCHAR(25)			NOT NULL,
 	price 			DOUBLE PRECISION	NOT NULL,
@@ -30,26 +30,26 @@ CREATE TABLE item (
 );
 
 CREATE TABLE order (
-	order_id		uuid				DEFAULT uuid_generate_v4();
-	item_id			uuid				FOREIGN KEY REFERENCES item(item_id),
+	order_id		UUID				NOT NULL IDENTITY PRIMARY KEY DEFAULT uuid_generate_v4(),
+	item_id			UUID				NOT NULL FOREIGN KEY REFERENCES item(item_id),
 	order_status	VARCHAR(25)			NOT NULL,
 	sub_total		DOUBLE PRECISION	NOT NULL,
 	total			DOUBLE PRECISION	NOT NULL
 );
 
 CREATE TABLE inventory (
-	inventory_id	uuid				DEFAULT uuid_generate_v4();
-	item_id			uuid				UNIQUE FOREIGN KEY REFERENCES item(item_id),
+	inventory_id	UUID				NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+	item_id			UUID				NOT NULL UNIQUE FOREIGN KEY REFERENCES item(item_id),
 	item_name		VARCHAR(50)			NOT NULL,
 	quantity		SMALLINT			NOT NULL
 );
 
 CREATE TABLE customer (
-	customer_id     uuid            DEFAULT uuid_generate_v4();
-	address_id      uuid            UNIQUE FOREIGN KEY REFERENCES address(address_id),
-	cart_id         uuid            UNIQUE FOREIGN KEY REFERENCES cart(cart_id),
-	creditcard_id   uuid            FOREIGN KEY REFERENCES creditcard(creditcard_id),
-	order_id        uuid            FOREIGN KEY REFERENCES order(order_id),
+	customer_id     UUID            NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+	address_id      UUID            NOT NULL UNIQUE FOREIGN KEY REFERENCES address(address_id),
+	cart_id         UUID            NOT NULL UNIQUE FOREIGN KEY REFERENCES cart(cart_id),
+	creditcard_id   UUID            FOREIGN KEY REFERENCES creditcard(creditcard_id),
+	order_id        UUID            FOREIGN KEY REFERENCES order(order_id),
 	email           VARCHAR(50)     NOT NULL,
 	first_name      VARCHAR(50)     NOT NULL,
 	last_name       VARCHAR(50)     NOT NULL,
